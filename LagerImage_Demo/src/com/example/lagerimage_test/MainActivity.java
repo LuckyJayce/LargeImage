fa073f0f78de3d9ddb91cc3036f43103a7f85efc
LuckyJayce <LuckyJayce@DESKTOP-S8DDLAO>
2015-10-25 23:25:25 +0800

    添加清明上河图

--- LagerImage_Demo/src/com/example/lagerimage_test/MainActivity.java
+++ LagerImage_Demo/src/com/example/lagerimage_test/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
 
 public class MainActivity extends Activity {
 	@Override
@@ -23,6 +25,14 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				startActivity(new Intent(getApplicationContext(), LongImageActivity.class));
+			}
+		});
+		
+		findViewById(R.id.button3).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getApplicationContext(), HActivity.class));
 			}
 		});
 	}
